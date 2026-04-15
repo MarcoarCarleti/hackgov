4738a0ba96845804df8531e6080aa5500325d5c5
MarcoarCarleti <marcarleti@gmail.com>
2026-04-14 22:37:51 -0300

    fix: cors config

--- backend/src/main/java/br/gov/hackgov/config/SecurityConfig.java
+++ backend/src/main/java/br/gov/hackgov/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "https://hackgov.vercel.app"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowCredentials(true);
